a56507ce990c791152c961fea67653ca227c582e
andreaem <info@andreaem.com>
2016-07-07 18:00:14 +0200

    MainActivity CardViews click implementation

--- app/src/main/java/it/fdc/dynamicbrand/MainActivity.java
+++ app/src/main/java/it/fdc/dynamicbrand/MainActivity.java
@@ -1,13 +1,32 @@
 package it.fdc.dynamicbrand;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.card1).setOnClickListener(this);
+        findViewById(R.id.card2).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+
+        switch (v.getId()) {
+            case R.id.card1:
+                intent = new Intent(this,GenerationActivity.class);
+                break;
+            case R.id.card2:
+                intent = new Intent(this,BrandsActivity.class);
+                break;
+        }
+        startActivity(intent);
     }
 }
