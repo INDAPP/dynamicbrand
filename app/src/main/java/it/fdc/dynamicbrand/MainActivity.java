0ac422097ea55bd5f62faad4b28bdd6254d35f2a
andreaem <info@andreaem.com>
2016-07-08 23:30:19 +0200

    MainActivity Review

--- app/src/main/java/it/fdc/dynamicbrand/MainActivity.java
+++ app/src/main/java/it/fdc/dynamicbrand/MainActivity.java
@@ -1,5 +1,6 @@
 package it.fdc.dynamicbrand;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,20 +12,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.card1).setOnClickListener(this);
-        findViewById(R.id.card2).setOnClickListener(this);
+
+        findViewById(R.id.btnBrands).setOnClickListener(this);
+        findViewById(R.id.btnContacts).setOnClickListener(this);
+        findViewById(R.id.btnGeneration).setOnClickListener(this);
+        findViewById(R.id.btnGuide).setOnClickListener(this);
+        findViewById(R.id.btnLicence).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         Intent intent = null;
-
         switch (v.getId()) {
-            case R.id.card1:
-                intent = new Intent(this,GenerationActivity.class);
+            case R.id.btnGeneration:
+                intent = new Intent(this, GenerationActivity.class);
                 break;
-            case R.id.card2:
+            case R.id.btnBrands:
                 intent = new Intent(this,BrandsActivity.class);
+                break;
+            case R.id.btnGuide:
+
+                break;
+            case R.id.btnContacts:
+
+                break;
+            case R.id.btnLicence:
+                intent = new Intent(this,LicencesActivity.class);
                 break;
         }
         startActivity(intent);
